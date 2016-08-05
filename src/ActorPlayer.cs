071c9a1c6c6ff2ba3e3e163752b09baf2cf61d23
Braeden Atlee <nedearb@gmail.com>
2016-08-05 14:55:21 -0700

    reduced size

--- src/ActorPlayer.cs
+++ src/ActorPlayer.cs
@@ -21,14 +21,14 @@ namespace Ageless {
 			legUpperLeft.matrix = Matrix4.CreateRotationX((float)Math.PI * 0.75f) * Matrix4.CreateTranslation(0, 0, 0);
 
 			Bone legLowerLeft = new Bone(legUpperLeft, ModelControl.getModel("limb"), 0);
-			legLowerLeft.matrix = Matrix4.CreateRotationX((float)Math.PI * 0.25f) * Matrix4.CreateTranslation(0, 3f, 0);
+			legLowerLeft.matrix = Matrix4.CreateRotationX((float)Math.PI * 0.25f) * Matrix4.CreateTranslation(0, 1.5f, 0);
 
 
 			Bone legUpperRight = new Bone(torso, ModelControl.getModel("limb"), 0);
 			legUpperRight.matrix = Matrix4.CreateRotationX((float)Math.PI * -0.75f) * Matrix4.CreateTranslation(0, 0, 0);
 
 			Bone legLowerRight = new Bone(legUpperRight, ModelControl.getModel("limb"), 0);
-			legLowerRight.matrix = Matrix4.CreateRotationX((float)Math.PI * -0.25f) * Matrix4.CreateTranslation(0, 3f, 0);
+			legLowerRight.matrix = Matrix4.CreateRotationX((float)Math.PI * -0.25f) * Matrix4.CreateTranslation(0, 1.5f, 0);
 
 			bones.Add(torso);
 			bones.Add(legUpperLeft);
